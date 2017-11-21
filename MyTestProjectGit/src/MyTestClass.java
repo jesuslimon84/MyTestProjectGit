9646b681511e3e469ab873a851a8c9f35ce7e52e
jesus.limon.guerrero <jesus.limon.guerrero@.accenture.com>
2017-11-21 16:10:35 -0600

    Test

--- MyTestProjectGit/src/MyTestClass.java
+++ MyTestProjectGit/src/MyTestClass.java
@@ -3,6 +3,7 @@ public class MyTestClass {
 	public static void main(String[] args) {
 		System.out.println("Hola Jesús");
 		System.out.println("Hola Javier");
+		System.out.println("Hola mundo");
 	}
 	
 }
