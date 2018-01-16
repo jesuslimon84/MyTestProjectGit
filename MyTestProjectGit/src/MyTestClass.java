16cc759dea5a74ab3b3c7b1d65961962946c84fe
Limon Guerrero, Jesus <jesus.limon.guerrero@.accenture.com>
2018-01-16 14:59:56 -0600

    Test 0

--- MyTestProjectGit/src/MyTestClass.java
+++ MyTestProjectGit/src/MyTestClass.java
@@ -1,9 +1,7 @@
 public class MyTestClass {
 
 	public static void main(String[] args) {
-		System.out.println("Hola Jesús");
-		System.out.println("Hola Javier");
-		System.out.println("Hola mundo");
+		System.out.println("Test 0");
 		System.out.println("Hola mundo de nuevo");
 	}
 	
